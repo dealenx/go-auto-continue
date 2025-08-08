af9b3355049229e72024d08524c56b3873e6c60f
Daniil Gorodilov <dealenx@gmail.com>
2025-08-08 18:42:48 +0700

    Добавлено уведомление "Hello World" и периодическое сообщение каждые 5 секунд в расширении. Реализована очистка таймера при деактивации.

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,19 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("Hello World 3");
+
+  // Показать уведомление с Hello World
+  vscode.window.showInformationMessage("Hello World! 1");
+
+  // Каждые 5 секунд показывать сообщение
+  const interval = setInterval(() => {
+    vscode.window.showInformationMessage("Hello World! 2");
+  }, 5000);
+
+  // Очистка таймера при деактивации
+  context.subscriptions.push({
+    dispose: () => clearInterval(interval),
+  });
 }
 
 export function deactivate() {
